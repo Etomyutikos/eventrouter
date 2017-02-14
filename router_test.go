2e553c9818965f8199d554bae799ba0464885969
Erik Pettis <etomyutikos@gmail.com>
2017-02-13 23:32:36 -0500

    remove superfluous tests

--- router_test.go
+++ router_test.go
@@ -6,50 +6,50 @@ import (
 	"testing"
 )
 
-func TestSingleHandlers(t *testing.T) {
+func TestHandlers(t *testing.T) {
 	tests := []struct {
-		desc        string
-		subscribeRt string
-		publishRt   string
+		desc         string
+		subscribeRts []string
+		publishRt    string
 	}{
 		{
 			"top-level event",
-			"event",
+			[]string{"event"},
 			"event",
 		},
 		{
 			"top-level wildcard event",
-			"*",
+			[]string{"*"},
 			"event",
 		},
 		{
 			"second-level event",
-			"first.second",
+			[]string{"first.second"},
 			"first.second",
 		},
 		{
 			"second-level wildcard first event",
-			"*.second",
+			[]string{"*.second"},
 			"first.second",
 		},
 		{
 			"second-level wildcard second event",
-			"first.*",
+			[]string{"first.*"},
 			"first.second",
 		},
 		{
-			"third-level wildcard first event",
-			"*.second.third",
+			"top-level event 2 handlers",
+			[]string{"event", "event"},
+			"event",
+		},
+		{
+			"3 2 1 wildcards",
+			[]string{"*", "*.*", "*.*.*"},
 			"first.second.third",
 		},
 		{
-			"third-level wildcard second event",
-			"first.*.third",
-			"first.second.third",
-		},
-		{
-			"third-level wildcard third event",
-			"first.second.*",
+			"top-level longer event",
+			[]string{"first"},
 			"first.second.third",
 		},
 	}
@@ -57,12 +57,11 @@ func TestSingleHandlers(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.desc, func(t *testing.T) {
 			r := New()
-
 			p := "payload"
 
-			var called bool
+			var called int
 			h := HandlerFunc(func(e Event) {
-				called = true
+				called++
 
 				expectedRt := strings.Split(test.publishRt, ".")
 				if !reflect.DeepEqual(e.Route, expectedRt) {
@@ -74,42 +73,19 @@ func TestSingleHandlers(t *testing.T) {
 				}
 			})
 
-			r.Subscribe(test.subscribeRt, h)
+			for _, rt := range test.subscribeRts {
+				r.Subscribe(rt, h)
+			}
+
 			r.Publish(test.publishRt, p)
 
-			if !called {
-				t.Fatal("handler was never called")
+			expectedCount := len(test.subscribeRts)
+			if called != expectedCount {
+				t.Fatalf("handler called count incorrect; expected: %d, actual: %d", expectedCount, called)
 			}
 		})
 	}
 }
 
-func TestMultipleHandlers(t *testing.T) {
-	r := New()
-
-	rt := "event"
-	p := "payload"
-
-	var called int
-	h := HandlerFunc(func(e Event) {
-		called += 1
-
-		expectedRt := []string{"event"}
-		if !reflect.DeepEqual(e.Route, expectedRt) {
-			t.Fatalf("incorrect route; expected: %v, actual: %v", expectedRt, e.Route)
-		}
-
-		if e.Payload != p {
-			t.Fatalf("incorrect payload; expected: %s, actual: %s", p, e.Payload)
-		}
-	})
-
-	r.Subscribe(rt, h)
-	r.Subscribe(rt, h)
-
-	r.Publish(rt, p)
-
-	if called != 2 {
-		t.Fatalf("handler called count incorrect; expected: %d, actual: %d", 2, called)
-	}
-}
+// three wildcards, only one publish part: panic
+// does branching work like I expect?
